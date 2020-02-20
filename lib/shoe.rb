ee14a447cab576665ce9a9076efb5f6788b11aa5
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-20 03:43:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,9 @@
 # Make your shoe class here!
 class Shoe 
+  attr_reader :brand
   def initialize(brand)
     @brand = brand 
   end 
   
-  def brand=(brand)
-    @brand = brand 
-  end 
-  
-  def brand
-    @brand
-  end 
+
 end
